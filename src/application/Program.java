e961cd2f9c6ed0b5820a1f44a5b25e0d0e2324bd
Lucas Dias de Almeida <lucasdias1589@gmail.com>
2021-02-08 16:04:59 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,14 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg Coll", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		
+		System.out.println("\n=== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Complete!");
+		
 	}
 
 }
